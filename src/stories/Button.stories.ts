185edfe8c41af365ae4a760fb960c3dee96e53fd
TLC 강동언 <soan32@hanmail.net>
2025-06-05 13:57:19 +0900

    feat: input storybook 구현

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,7 +1,5 @@
-// src/components/Button/Button.stories.tsx
-import type { Meta, StoryObj } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import { Button } from './Button';
-import { Search } from 'lucide-react';
 
 const meta: Meta<typeof Button> = {
   title: 'Components/Button',
