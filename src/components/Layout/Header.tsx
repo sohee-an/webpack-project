ccb11b1426459aa4a46b173fb3eb7a6ed1ed83a9
TLC 강동언 <soan32@hanmail.net>
2025-06-05 10:14:37 +0900

    feat: 검색어 인풋 ui 구현

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Button from '../share/Button';
 import MenuTab from './MenuTab';
+import Input from '../share/Input';
+import { Search } from 'lucide-react';
 
 const tab_list = ['구독', '개별 구매'];
 
@@ -9,6 +11,10 @@ function Header() {
     <header className="flex items-center justify-between p-2 border-b border-gray-700">
       <MenuTab tabs={tab_list} />
       <div className="flex items-center space-x-4">
+        <Input
+          placeholder="검색어를 입력하세요"
+          icon={<Search className="w-4 h-4 text-white mr-2" />}
+        />
         <Button size="sm">로그인</Button>
         <Button size="sm">회원가입</Button>
       </div>
