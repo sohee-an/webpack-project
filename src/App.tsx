d77b59a998ec60c86d6c1703e0906b2d07df3d13
sohee-an <soan32@hanmail.net>
2025-06-06 12:54:34 +0900

    chore: 파일이름수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Layout from './components/Layout/\bLayout';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
