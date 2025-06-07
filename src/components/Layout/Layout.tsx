43774e823e306d4b2e7c00429054c7818d701b74
sohee-an <soan32@hanmail.net>
2025-06-07 21:02:49 +0900

    feat: footer 구현

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
 
 function Layout({ children }: { children?: React.ReactNode }) {
   return (
     <div className="min-h-screen w-screen flex flex-col bg-black ">
       <Header />
       <div>{children}</div>
+      <Footer />
     </div>
   );
 }
