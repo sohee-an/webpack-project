7590bf91f3860c11cdb9a527214b5b8d55c0af7c
sohee-an <soan32@hanmail.net>
2025-06-07 11:10:38 +0900

    feat: popluarCard ui 수정

--- src/components/share/MovieCard/MovieCard.tsx
+++ src/components/share/MovieCard/MovieCard.tsx
@@ -3,11 +3,19 @@ import { TMovie } from '@/hooks/useMoviePopular';
 
 function MovieCard({ item }: { item: TMovie }) {
   return (
-    <div className="flex flex-col gap-1 text-white">
+    <div className="flex flex-col gap-1 text-white cursor-pointer">
       <div className="text-sm text-gray-400">{item.title}</div>
       <div>{item.original_title}</div>
       <div className="text-gray-400">{item.overview}</div>
-      <img src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt="${movie.title} Poster" />
+
+      <div className="relative group">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+          alt={`${item.title} Poster`}
+          className="w-full h-auto object-cover"
+        />
+        <div className="absolute inset-0 bg-black bg-opacity-30 opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+      </div>
     </div>
   );
 }
