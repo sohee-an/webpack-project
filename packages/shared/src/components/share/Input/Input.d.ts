ed71ea3a850cad4d2619f127b3595ea66c631ec6
sohee-an <soan32@hanmail.net>
2025-07-06 17:55:36 +0900

    refactor: 파일 삭제, tsconfig 부분 설정

---
+++ packages/shared/src/components/share/Input/Input.d.ts
@@ -0,0 +1,9 @@
+import React, { ReactNode } from 'react';
+export declare const Input: React.ForwardRefExoticComponent<
+  {
+    icon?: ReactNode;
+    className?: string;
+  } & React.InputHTMLAttributes<HTMLInputElement> &
+    React.RefAttributes<HTMLInputElement>
+>;
+//# sourceMappingURL=Input.d.ts.map
---
+++ packages/shared/src/utiles/index.d.ts
@@ -0,0 +1,5 @@
+import { type ClassValue } from 'clsx';
+export declare function cn(...inputs: ClassValue[]): string;
+declare const camelizeKeys: (input: unknown) => unknown;
+export { camelizeKeys };
+//# sourceMappingURL=index.d.ts.map
