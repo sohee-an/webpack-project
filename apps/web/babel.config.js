f3cb345042012fa98b304c633f4d674fa3ff2c04
sohee-an <soan32@hanmail.net>
2025-07-25 12:38:09 +0900

    fix: babel.config.js 이동

---
+++ apps/web/babel.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env'],
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic',
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        corejs: 3,
+        regenerator: true,
+        helpers: true,
+      },
+    ],
+  ],
+};
