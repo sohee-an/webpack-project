9abff1010301c72594ad4e6d170dd242247f4f34
sohee-an <soan32@hanmail.net>
2025-09-09 16:31:04 +0900

    fix: 주석처리 삭제

--- apps/next_web/next.config.ts
+++ apps/next_web/next.config.ts
@@ -1,25 +1,23 @@
-// next.config.ts
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 
-  // ✅ Next/Image 최적화: 원격 이미지 도메인 허용 + 포맷
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'image.tmdb.org', // TMDB 이미지 서버
+        hostname: 'image.tmdb.org',
         pathname: '/t/p/**',
       },
     ],
   },
 
-  // ✅ 모노레포(shared) 소스도 함께 트랜스파일
+  //  모노레포(shared) 소스도 함께 트랜스파일
   transpilePackages: ['@packages/shared'],
 
-  // ✅ 모노레포 외부 디렉토리 read 허용
+  // 모노레포 외부 디렉토리 read 허용
   experimental: { externalDir: true },
 };
 
